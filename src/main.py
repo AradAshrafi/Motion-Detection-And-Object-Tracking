e9a22ce0d8e7e7e6f95680da1e4e9cccbdd49c6d
Arad.Ashrafi@gmail.com <Arad.Ashrafi@gmail.com>
2019-01-23 06:33:27 +0330

    saving captured and shown frames(from webcam or loacal video) to output.avi

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.ObjectsStateHandler import ObjectsStateHandler
 from src.lib.lib import INPUT_VIDEO_LOC
+from src.lib.lib import SAVING_OUTPUT_VIDEO_LOC
 import cv2
 
 
@@ -9,6 +10,14 @@ import cv2
 def __get_video_mode():
     user_selected_mode = int(input("select your mode :\n1 - webcam\n2 - dataset video\n"))
     return user_selected_mode
+
+
+# create video writer to write whole process with it
+def __create_video_writer():
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # we know that the frame size width is 640 and height is 480
+    # ------important reminder------- if frame size changed,note that you must change 640,480 in following sentence too
+    return cv2.VideoWriter(SAVING_OUTPUT_VIDEO_LOC, fourcc, 20.0, (640, 480))
 
 
 def main():
@@ -25,9 +34,12 @@ def main():
         cap = cv2.VideoCapture(0)
     else:
         cap = cv2.VideoCapture(INPUT_VIDEO_LOC)
+
+    # get video writer in 640*480 size (matches frame size)
+    video_writer = __create_video_writer()
+
     # foreground background separator
     fgbg = cv2.createBackgroundSubtractorKNN(history=1000)
-
     # in web cam mode it will loop until user get tired :D
     # in offline mode it will loop until video finished
     while True:
@@ -41,7 +53,7 @@ def main():
         state.update(frame=frame, foreground_mask=fg_mask)
 
         cv2.imshow("snowish :D", frame)  # show image with snowflakes effect on i :D
-
+        video_writer.write(frame)
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break
